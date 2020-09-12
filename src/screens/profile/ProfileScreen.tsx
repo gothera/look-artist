e949a167b62f547a4daf178feeb188cf36b1e7a1
sabin10 <hantusabin10@gmail.com>
2020-09-12 18:05:15 +0300

    logut btn

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { resetGenericPassword } from 'react-native-keychain';
+import { pushAuthScreen } from '../../navigation';
 
 const ProfileScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>profile screen</Text>
+      <TouchableHighlight
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingHorizontal: 30,
+          paddingVertical: 10,
+          backgroundColor: 'red',
+        }}
+        onPress={() => {
+          resetGenericPassword();
+          pushAuthScreen();
+        }}
+      >
+        <Text>PRESS LOGOUT</Text>
+      </TouchableHighlight>
     </View>
   );
 };
